abe0a8fcff7caadbed18f2f958b8a19757547a0e
JeradeaSimmons <JeradeaSimmons@gmail.com>
2022-12-20 16:58:53 -0700

    h

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React from "react"
 
 
-const App = ()=> (
-  <div>
-    <p>
-      Hello World
-    </p>
-  </div>
+const App = ()=> {
+  const now = new Date()
+  const a = 10
+  const b = 20
+
+
+  return React.createElement(
+ 'div',
+ null,
+ React.createElement(
+  'p', null, 'Hello world, it is', now.toString()
+ ),
+ React.createElement(
+  'p', null, a, ' plus ', b, ' is ', a + b
+ )
 )
-
+  }
 export default App;
