77f1d4aaad87704416ae86945c049f0be8ea2765
JeradeaSimmons <JeradeaSimmons@gmail.com>
2022-12-21 14:00:33 -0700

    more practice

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from "react"
 
-
+const Hello = (props) => {
+  return(
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}
 const App = ()=> {
-  const now = new Date()
-  const a = 10
-  const b = 20
-
-
-  return React.createElement(
- 'div',
- null,
- React.createElement(
-  'p', null, 'Hello world, it is', now.toString()
- ),
- React.createElement(
-  'p', null, a, ' plus ', b, ' is ', a + b
- )
+return(
+  <div>
+    <h1>Greetings</h1>
+    <Hello name='George'/>
+    <Hello name='Daisy' />
+  </div>
 )
   }
 export default App;
